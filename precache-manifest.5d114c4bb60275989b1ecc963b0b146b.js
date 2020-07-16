bdf96ab678453743f897fdfc1e177c0981628bd2
robbart-cy8 <64191721+robbart-cy8@users.noreply.github.com>
2020-07-16 18:03:11 +0530

    Updates

---
+++ precache-manifest.5d114c4bb60275989b1ecc963b0b146b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26742df698453831d35aae0463173514",
+    "url": "/robbfri/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robbfri/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d0f2637008def9e51abc",
+    "url": "/robbfri/static/css/main.6f1215c1.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robbfri/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robbfri/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f2637008def9e51abc",
+    "url": "/robbfri/static/js/main.1a5c6020.chunk.js"
+  },
+  {
+    "revision": "f9c803b4eaeaca79ca0a",
+    "url": "/robbfri/static/js/runtime-main.fda2c3bb.js"
+  }
+]);
